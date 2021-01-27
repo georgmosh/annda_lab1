a63130186b34e146c51740cfa6f23e1cc9634a0e
georgmosh <georgiosmoshovis@gmail.com>
2021-01-28 00:34:37 +0100

    Two-layered Multilayer Perceptron

--- demo_mlp.py
+++ demo_mlp.py
@@ -15,7 +15,7 @@ def generateDatapoints():
     sigma_A = 0.2
 
     means_B = np.zeros(2)
-    means_B -= 0.1
+    means_B[0] -= 0.1
 
     sigma_B = 0.3
 
@@ -55,7 +55,7 @@ def drawLine2P(x,y,xlims):
 def plot_loss(loss_train, loss_val, num_epochs):
     epochs = range(1, num_epochs + 1)
     plt.plot(epochs, loss_train, 'g', label='Training loss')
-    plt.plot(epochs, loss_val, 'b', label='validation loss')
+   # plt.plot(epochs, loss_val, 'b', label='validation loss')
     plt.title('Training and Validation loss')
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
@@ -76,7 +76,10 @@ def forward_pass(x, W1, W2):
     o1 = activation(h1)
     
     # hidden layer
-    h2 = np.matmul(W2, h1)
+    O1 = np.ones([o1.shape[0] + 1, o1.shape[1]])
+    O1[:-1, :] = o1
+    o1 = O1
+    h2 = np.matmul(W2, o1)
     o2 = activation(h2)
     
     return h1, o1, h2, o2
@@ -84,31 +87,35 @@ def forward_pass(x, W1, W2):
 def backward_pass(lr, x, h1, o1, h2, o2, t, W1, W2):
     # hidden layer
     delta_hidden = np.multiply((o2[0] - t), activation_der(o2[0]))
-    dW = -lr * np.matmul(delta_hidden, np.transpose(o1))
-    W2[0] = W2[0] + dW
+    dW2 = -lr * np.matmul(delta_hidden, np.transpose(o1))
     
     # output layer
     delta_hidden = delta_hidden.reshape(len(delta_hidden), 1)
-    delta_output = np.multiply(np.matmul(np.transpose(W2).shape,  np.transpose(delta_hidden).shape), activation_der(o1))
-    dW = -lr * np.matmul(delta_output, x)
-    W1 = W1 + dW
+    # delta_output = np.multiply(np.matmul(np.transpose(W2).shape,  np.transpose(delta_hidden).shape), activation_der(o1))
+    delta_output = np.multiply(np.matmul(np.transpose(W2),  np.transpose(delta_hidden)), activation_der(o1))
+    dW1 = -lr * np.matmul(delta_output, x)
+
+    # weight update
+    W2[0] = W2[0] + dW2
+    W1 = W1 + dW1[:len(dW1)-1]
+
+    return W1, W2
 
 def MSE(t, o2):
     loss = 0.5 * np.dot((t - o2), (t - o2))
     return loss
 
 def mlp(hidden_nodes):
-    num_epochs = 10
-    num_classes = 2
-    lr = 0.3
+    num_epochs = 100
+    lr = 0.01
     num_of_datapoints, class_A, class_B = generateDatapoints()
     x_axis = [class_A[0], class_B[0]]
     y_axis = [class_A[1], class_B[1]]
     losses = np.zeros(num_epochs)
     val_losses = np.zeros(num_epochs)
-    num_crossval_runs = 5
+    num_crossval_runs = 1
     
-    class_A_all = [class_A[0], class_A[1], np.zeros(int(num_of_datapoints/2))]
+    class_A_all = [class_A[0], class_A[1], -np.ones(int(num_of_datapoints/2))]
     class_B_all = [class_B[0], class_B[1], np.ones(num_of_datapoints)]
 
 #    plot(class_A[0], class_A[1], class_B[0], class_B[1])    
@@ -117,14 +124,14 @@ def mlp(hidden_nodes):
     target_whole = dataset[:,2].copy()
     dataset[:,2] = 1
 
-    d_train = int(0.8 * num_of_datapoints)
-    data = dataset[0:d_train,:] 
-    val_data = dataset[d_train:,:]
-    target = target_whole[0:d_train]
-    val_target = target_whole[d_train:]
+    #d_train = int(0.8 * 2 * num_of_datapoints)
+    data = dataset#[0:d_train,:]
+  #  val_data = dataset[d_train:,:]
+    target = target_whole#[0:d_train]
+  #  val_target = target_whole[d_train:]
     
     for _ in range(0, num_crossval_runs):
-        W1 = np.random.rand(hidden_nodes, 3)
+        W1 = np.random.rand(hidden_nodes - 1, 3)
         W2 = np.random.rand(1, hidden_nodes)
         
         for epoch in range(0, num_epochs):
@@ -134,18 +141,19 @@ def mlp(hidden_nodes):
             loss = MSE(target, o2[0])
             losses[epoch] += loss
         
-            backward_pass(lr, data, h1, o1, h2, o2, target, W1, W2)
-    
+            W1, W2 = backward_pass(lr, data, h1, o1, h2, o2, target, W1, W2)
+
+            print("hello")
             ### --------------- VALIDATION --------------- ##
-            h1, o1, h2, o2 = forward_pass(val_data, W1, W2)
-        
-            loss = MSE(val_target, o2[0])
-            val_losses[epoch] += loss
+            # h1, o1, h2, o2 = forward_pass(val_data, W1, W2)
+            #
+            # loss = MSE(val_target, o2[0])
+            # val_losses[epoch] += loss
     
     losses /= num_crossval_runs
-    val_losses /= num_crossval_runs
+   # val_losses /= num_crossval_runs
     losses = np.ndarray.tolist(losses)
-    val_losses = np.ndarray.tolist(val_losses)
+    val_losses = []#np.ndarray.tolist(val_losses)
     
     ### --------------- EVALUATION --------------- ##
     print("Average loss per epoch", losses, val_losses)
