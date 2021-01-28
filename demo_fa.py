243fb74f56be0ff14c0454a8c0cf0ee4eac4e5af
georgmosh <georgiosmoshovis@gmail.com>
2021-01-28 01:51:54 +0100

    Function approximation with two-layered Multilayer Perceptron

--- demo_fa.py
+++ demo_fa.py
@@ -5,15 +5,12 @@ import pandas as pd
 import math
 
 def generateDatapoints():
-    np.random.seed(0)
-    
+    np.random.seed(72)
+
     num_of_datapoints = 441
 
     data = []
-    x_axis = []
-    y_axis = []
-    expectation = []
-    
+
     for i in np.arange(-5, 5.5, 0.5):
         for j in np.arange(-5, 5.5, 0.5):
             f = np.exp(-0.1 * np.dot(i, i)) * np.exp(-0.1 * np.dot(j, j))
@@ -22,29 +19,56 @@ def generateDatapoints():
             elem[1] += j
             elem[2] += f
             data.append(elem)
-            x_axis.append(i)
-            y_axis.append(j)
-            expectation.append(f)
-            
-    data = np.array(data)
-    x_axis = np.array(x_axis)
-    y_axis = np.array(y_axis)
-    expectation = np.array(expectation)
 
-    return num_of_datapoints, data, x_axis, y_axis, expectation
-     
+    data = np.array(data)
+
+    return num_of_datapoints, data
+
+def plot(X, y, X1, y1):
+    plt.scatter(X, y, color='r')
+    plt.scatter(X1, y1, color='g')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
+
+def plot_with_boundary(X, y, X1, y1, p1, p2):
+    plt.scatter(X, y, color='r')
+    plt.scatter(X1, y1, color='g')
+    drawLine2P(p1, p2, [-2.5, 1.5])
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
+
+def drawLine2P(x, y, xlims):
+    # Source:
+    # https://stackoverflow.com/questions/9148927/matplotlib-extended-line-over-2-control-points
+    xrange = np.arange(xlims[0], xlims[1], 0.1)
+    A = np.vstack([x, np.ones(len(x))]).T
+    k, b = np.linalg.lstsq(A, y)[0]
+    plt.plot(xrange, k * xrange + b)
+
 def plot_loss(loss_train, loss_val, num_epochs):
     epochs = range(1, num_epochs + 1)
     plt.plot(epochs, loss_train, 'g', label='Training loss')
     plt.plot(epochs, loss_val, 'b', label='validation loss')
-    plt.title('Training and Validation loss')
+    plt.title('Training vs Validation loss')
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
     plt.legend()
-    plt.show()    
- 
+    plt.show()
+
+def plot_accuracy(acc_train, acc_val, num_epochs):
+    epochs = range(1, num_epochs + 1)
+    plt.plot(epochs, acc_train, 'g', label='Training accuracy')
+    plt.plot(epochs, acc_val, 'b', label='validation accuracy')
+    plt.title('Training vs Validation accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+
 def activation(x):
-    out = 2/(1 + np.exp(-x)) - 1
+    out = 2 / (1 + np.exp(-x)) - 1
     return out
 
 def activation_der(x):
@@ -55,84 +79,97 @@ def forward_pass(x, W1, W2):
     # input layer
     h1 = np.matmul(W1, np.transpose(x))
     o1 = activation(h1)
-    
+
     # hidden layer
-    h2 = np.matmul(W2, h1)
+    O1 = np.ones([o1.shape[0] + 1, o1.shape[1]])
+    O1[:-1, :] = o1
+    o1 = O1
+    h2 = np.matmul(W2, o1)
     o2 = activation(h2)
-    
+
     return h1, o1, h2, o2
 
 def backward_pass(lr, x, h1, o1, h2, o2, t, W1, W2):
     # hidden layer
     delta_hidden = np.multiply((o2[0] - t), activation_der(o2[0]))
-    dW = -lr * np.matmul(delta_hidden, np.transpose(o1))
-    W2[0] = W2[0] + dW
-    
+    dW2 = -lr * np.matmul(delta_hidden, np.transpose(o1))
+
     # output layer
     delta_hidden = delta_hidden.reshape(len(delta_hidden), 1)
-    delta_output = np.multiply(np.matmul(np.transpose(W2).shape,  np.transpose(delta_hidden).shape), activation_der(o1))
-    dW = -lr * np.matmul(delta_output, x)
-    W1 = W1 + dW
+    # delta_output = np.multiply(np.matmul(np.transpose(W2).shape,  np.transpose(delta_hidden).shape), activation_der(o1))
+    delta_output = np.multiply(np.matmul(np.transpose(W2), np.transpose(delta_hidden)), activation_der(o1))
+    dW1 = -lr * np.matmul(delta_output, x)
+
+    # weight update
+    W2[0] = W2[0] + dW2
+    W1 = W1 + dW1[:len(dW1) - 1]
+
+    return W1, W2
 
 def MSE(t, o2):
-    loss = 0.5 * np.dot((t - o2), (t - o2))
+    loss = np.dot((t - o2), (t - o2)) / t.shape[0]
     return loss
 
 def mlp(hidden_nodes):
-    num_epochs = 50
-    num_classes = 2
-    lr = 0.3
-    num_of_datapoints, dataset, x_axis, y_axis, expectation = generateDatapoints()
+    num_epochs = 250
+    lr = 0.01
+    num_of_datapoints, dataset = generateDatapoints()
     losses = np.zeros(num_epochs)
     val_losses = np.zeros(num_epochs)
-    num_crossval_runs = 5
-    
+    accuracies = np.zeros(num_epochs)
+    val_accuracies = np.zeros(num_epochs)
+    num_crossval_runs = 1
+
     np.random.shuffle(dataset)
-    target_whole = dataset[:,2].copy()
-    dataset[:,2] = 1
-    
+    target_whole = dataset[:, 2].copy()
+    dataset[:, 2] = 1
+
     d_train = int(0.8 * num_of_datapoints)
-    data = dataset[0:d_train,:] 
-    val_data = dataset[d_train:,:]
+    data = dataset[0:d_train, :]
+    val_data = dataset[d_train:, :]
     target = target_whole[0:d_train]
     val_target = target_whole[d_train:]
-    
+
     for _ in range(0, num_crossval_runs):
-        W1 = np.random.rand(hidden_nodes, 3)
+        W1 = np.random.rand(hidden_nodes - 1, 3)
         W2 = np.random.rand(1, hidden_nodes)
-        
+
         for epoch in range(0, num_epochs):
             ### --------------- TRAINING --------------- ##
             h1, o1, h2, o2 = forward_pass(data, W1, W2)
-        
+
             loss = MSE(target, o2[0])
             losses[epoch] += loss
-        
-            backward_pass(lr, data, h1, o1, h2, o2, target, W1, W2)
-    
+
+            accuracy = np.sum(np.abs(o2 - target) < 1) / target.shape[0]
+            accuracies[epoch] += accuracy
+
+            W1, W2 = backward_pass(lr, data, h1, o1, h2, o2, target, W1, W2)
+
             ### --------------- VALIDATION --------------- ##
             h1, o1, h2, o2 = forward_pass(val_data, W1, W2)
-        
+
             loss = MSE(val_target, o2[0])
             val_losses[epoch] += loss
-    
+
+            accuracy = np.sum(np.abs(o2 - val_target) < 1) / val_target.shape[0]
+            val_accuracies[epoch] += accuracy
+
     losses /= num_crossval_runs
     val_losses /= num_crossval_runs
+    accuracies /= num_crossval_runs
+    val_accuracies /= num_crossval_runs
     losses = np.ndarray.tolist(losses)
     val_losses = np.ndarray.tolist(val_losses)
-    
+    accuracies = np.ndarray.tolist(accuracies)
+    val_accuracies = np.ndarray.tolist(val_accuracies)
+
     ### --------------- EVALUATION --------------- ##
     print("Average loss per epoch", losses, val_losses)
     plot_loss(losses, val_losses, num_epochs)
-    
-# --------------------- garbage
-#    for epoch in range(0, num_epochs):
-#        delta = -lr * np.dot((np.dot(np.transpose(W), np.transpose(data)) - target), data)
-#        W = W + delta
 
-#    point_1 = [0, (-W[2]/W[0])]
-#    point_2 = [(-W[2]/W[1]), 0]
-#    plot_with_boundary(class_A[0], class_A[1], class_B[0], class_B[1], point_1, point_2)
-#    print(W)
+    print("Average accuracy per epoch", accuracies, val_accuracies)
+    plot_accuracy(accuracies, val_accuracies, num_epochs)
 
-mlp(100)
+
+mlp(3)
